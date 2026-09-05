19dac3b39e8d3f49ed56f6abb6187c761ae9f9c1
Geórgia Pisoni Bender Andrade <georgiapisonibenderandrade@Georgias-MacBook-Air.local>
2026-09-05 20:01:40 +0200

    Add serial progress to timed MOSFET test

--- INA219/2026_rev-4ch/MOSFET_Timed_Test/MOSFET_Timed_Test.ino
+++ INA219/2026_rev-4ch/MOSFET_Timed_Test/MOSFET_Timed_Test.ino
@@ -9,6 +9,18 @@ const unsigned long HOLD_LOAD_MS  = 15000;
 const unsigned long HOLD_SHORT_MS = 5000;
 const unsigned long DEAD_TIME_MS  = 100;
 
+void timedWait(unsigned long duration)
+{
+  unsigned long started = millis();
+  unsigned long lastReport = started;
+  while (millis() - started < duration) {
+    if (millis() - lastReport >= 1000) {
+      lastReport = millis();
+      Serial.println(F("  test still running"));
+    }
+  }
+}
+
 void allOff()
 {
   for (uint8_t i = 0; i < 4; i++) {
@@ -19,41 +31,60 @@ void allOff()
 
 void testLoad(uint8_t channel)
 {
+  Serial.print(F("CH"));
+  Serial.print(channel + 1);
+  Serial.println(F(" LOAD ON (15 s)"));
   allOff();
   digitalWrite(LOAD_PINS[channel], HIGH);
   digitalWrite(LED_PIN, HIGH);
-  delay(HOLD_LOAD_MS);
+  timedWait(HOLD_LOAD_MS);
   digitalWrite(LOAD_PINS[channel], LOW);
   digitalWrite(LED_PIN, LOW);
-  delay(DEAD_TIME_MS);
+  Serial.print(F("CH"));
+  Serial.print(channel + 1);
+  Serial.println(F(" LOAD OFF"));
+  timedWait(DEAD_TIME_MS);
 }
 
 void testShort(uint8_t channel)
 {
+  Serial.print(F("CH"));
+  Serial.print(channel + 1);
+  Serial.println(F(" SHORT ON (5 s)"));
   allOff();
-  delay(DEAD_TIME_MS);
+  timedWait(DEAD_TIME_MS);
   digitalWrite(SHORT_PINS[channel], HIGH);
   digitalWrite(LED_PIN, HIGH);
-  delay(HOLD_SHORT_MS);
+  timedWait(HOLD_SHORT_MS);
   digitalWrite(SHORT_PINS[channel], LOW);
   digitalWrite(LED_PIN, LOW);
-  delay(DEAD_TIME_MS);
+  Serial.print(F("CH"));
+  Serial.print(channel + 1);
+  Serial.println(F(" SHORT OFF"));
+  timedWait(DEAD_TIME_MS);
 }
 
 void setup()
 {
+  Serial.begin(9600);
+  Serial.println(F("Timed MOSFET test starting"));
+  Serial.println(F("Load stages: 15 s; short stages: 5 s"));
   pinMode(LED_PIN, OUTPUT);
   for (uint8_t i = 0; i < 4; i++) {
     pinMode(SHORT_PINS[i], OUTPUT);
     pinMode(LOAD_PINS[i], OUTPUT);
   }
   allOff();
+  Serial.println(F("All MOSFET outputs OFF"));
 }
 
 void loop()
 {
+  Serial.println(F("--- LOAD TESTS ---"));
   for (uint8_t i = 0; i < 4; i++) testLoad(i);
+  Serial.println(F("--- SHORT TESTS ---"));
   for (uint8_t i = 0; i < 4; i++) testShort(i);
   allOff();
-  delay(5000);
+  Serial.println(F("Cycle complete; all MOSFET outputs OFF"));
+  timedWait(5000);
 }
